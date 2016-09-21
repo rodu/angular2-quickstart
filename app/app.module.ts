423e9ea5f08341458f654a638bc5b408b92e5763
rodu <greenheron35@gmail.com>
2016-09-21 19:55:21 +0100

    Adds a sample dashboard component

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Dashboard } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    Dashboard
+   ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {}
---
+++ app/dashboard/dashboard.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dashboard',
+  template: '<h1>I\'m the dashboard</h1>'
+})
+export class Dashboard {}
